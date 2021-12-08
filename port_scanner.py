56065ed2490922a917a8f122dcb781414d0c3e79
Lucy Groves <lucygroves@Lucys-MacBook-Air.local>
2021-12-08 16:28:46 -0500

    finished SYN/FIN scans

--- port_scanner.py
+++ port_scanner.py
@@ -2,6 +2,7 @@ from scapy.all import *
 import sys
 import random
 import argparse
+import time
 
 # https://nickmccullum.com/python-command-line-arguments
 # https://docs.python.org/3/library/argparse.html#module-argparse
@@ -17,21 +18,23 @@ def SYN_scan(ip_addr, ports, order="sequential"):
     """
     open_ports = []
     if order == "random":
-        ports = random.shuffle(ports)
+        random.shuffle(ports)
     elif order == "sequential":
         pass
     else:
         raise Exception("Invalid ports selected")
-    print("ports: ", ports)
+    
     for i in ports:
-        print(i)
-        resp = sr1(IP(dst=ip_addr)/TCP(dport=i, flags='S'), timeout=.5, verbose=0)     
+        # print(i)
+        resp = sr1(IP(dst=ip_addr)/TCP(dport=i, flags='S'), timeout=.02, verbose=0)     
         try: 
-            print(resp.sprintf('%TCP.src% \t %TCP.sport% \t %TCP.flags%'))
-            open_ports.append(i)
+            # print(resp.sprintf('%TCP.src% \t %TCP.sport% \t %TCP.flags%'))
+            if resp.getlayer(TCP).flags == 0x12:
+                open_ports.append(i)
+            sr1(IP(dst=ip_addr)/TCP(dport=i, flags='R'), timeout=.02, verbose=0) 
         except: 
             pass
-    #print(open_ports)
+    print(open_ports)
     return open_ports
 
 
@@ -41,27 +44,27 @@ def FIN_scan(ip_addr, ports, order="sequential"):
     input: target ip address, list of ports to scan, and port scanning order
     output: list of open ports by number, total ports scanned
     description: FIN scan the given ip address at each port by sending a FIN
-    packet and watching for an RST packet in respose (indicates the port is live)
+    packet and watching for an RST packet in respose (indicates the port is 
+    closed). An open port does not respond to a TCP FIN message
     """
     
     open_ports = []
     if order == "random":
-        ports = random.shuffle(ports)
+        random.shuffle(ports)
     elif order == "sequential":
         pass
     else:
         raise Exception("Invalid ports selected")
-    print("ports: ", ports)
+    # print("ports: ", ports)
     for i in ports:
-        print(i)
-        resp = sr1(IP(dst=ip_addr)/TCP(dport=i, flags='F'), timeout=.5, verbose=0)     
-        try: 
-            print(resp.sprintf('%TCP.src% \t %TCP.sport% \t %TCP.flags%'))
-            if resp.getlayer(TCP).flags == 0x12:
-                open_ports.append(i)
-            sr1(IP(dst=ip_addr)/TCP(dport=i, flags='R'), timeout=.5, verbose=0)   
-        except: 
+        # print(i)
+        resp = sr1(IP(dst=ip_addr)/TCP(dport=i, flags='F'), timeout=.02, verbose=0)     
+        if resp:
+            # if the port responds, that means it is closed
             pass
+        else: 
+            # an open port will not respond to a FIN packet
+            open_ports.append(i)
     print(open_ports)
     return open_ports
 
@@ -81,19 +84,31 @@ if __name__ == "__main__":
 
     # validate user input
     if (mode != "SYN" and mode != "FIN" and mode != "normal"):
-         raise argparse.ArgumentError(mode, "Invalid mode selected!")
+        print("Invalid mode selected!")
+        quit()
     if (ports != "common" and ports != "all"):
-         raise argparse.ArgumentError(ports, "Invalid port option selected!")
+        print("Invalid port option selected!")
+        quit()
     if (order != "random" and order != "sequential"):
-         raise argparse.ArgumentError(order, "Invalid order selected!")    
+        print("Invalid order selected!")  
+        quit()  
     if ports == "common":
         port_nums = common_ports
 
     print("starting port scan at address {}, scanning {} ports in {} order".format(target_ip, ports, order))
     if mode == "SYN":
-       SYN_scan(target_ip, port_nums, order)
-       # SYN_scan("131.229.72.13", port_nums, order)
+        start = time.time()
+        open_ports = SYN_scan(target_ip, port_nums, order)
+        stop = time.time()
+        execution_time = stop - start
+        # Kathleen feel free to get rid of this
+        print(execution_time)
     elif mode == "FIN":
-        FIN_scan(target_ip, port_nums, order)
+        start = time.time()
+        open_ports = FIN_scan(target_ip, port_nums, order)
+        stop = time.time()
+        execution_time = stop - start
+        print(execution_time)
+    
 
 
